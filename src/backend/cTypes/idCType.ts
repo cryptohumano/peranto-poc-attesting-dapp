f31f07d2c54392c471e3dcb0241e39c2bae6f6e2
Metabaron <axnotliztac@gmail.com>
2023-06-29 19:11:27 -0700

    add id temp ctype

---
+++ src/backend/cTypes/idCType.ts
@@ -0,0 +1,16 @@
+import type { ICType } from '@kiltprotocol/sdk-js';
+
+export const idCType: ICType = {
+  $schema: 'http://kilt-protocol.org/draft-01/ctype#',
+  properties: {
+    age: {
+      type: 'integer',
+    },
+    name: {
+      type: 'string',
+    },
+  },
+  title: 'Drivers License(wanghui)',
+  type: 'object',
+  $id: 'kilt:ctype:0x3112e1e3fb387e5eb6c109aa45afc7ed9df01f1c90a976a8b00585abf817ca82',
+};
--- src/backend/utilities/supportedCTypes.ts
+++ src/backend/utilities/supportedCTypes.ts
@@ -1,19 +1,21 @@
 import type { ICType } from '@kiltprotocol/sdk-js';
 
 import { emailCType } from '../cTypes/emailCType';
-
 import { twitterCType } from '../cTypes/twitterCType';
+import { idCType } from '../cTypes/idCType';
 
-export const supportedCTypeKeys = ['email', 'twitter'] as const;
+export const supportedCTypeKeys = ['email', 'twitter', 'id'] as const;
 
 export type SupportedCType = (typeof supportedCTypeKeys)[number];
 
 export const supportedCTypes: Record<SupportedCType, ICType> = {
   email: emailCType,
   twitter: twitterCType,
+  id: idCType,
 };
 
 export const kiltCost: Record<SupportedCType, number> = {
+  id: 1,
   email: 2,
   twitter: 3,
 };
