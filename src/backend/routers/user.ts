e346a38e7bc68b3ceb03d89caa08c279b3d5cbaf
metabaronespinosa <axnotliztac@gmail.com>
2023-07-01 15:59:39 -0600

    test fb 4

--- src/backend/routers/user.ts
+++ src/backend/routers/user.ts
@@ -1,8 +1,11 @@
 import express, { Router } from 'express';
+
 import { pay } from '../endpoints/user/pay';
 import { requestAttestation } from '../endpoints/user/requestAttestation';
 import { session } from '../endpoints/user/session';
 import { terms } from '../endpoints/user/terms';
+import { metamap } from '../endpoints/user/metamap';
+
 import { configuration } from '../utilities/configuration';
 
 export const userRouter = Router();
@@ -11,6 +14,7 @@ userRouter.use(session);
 userRouter.use(terms);
 userRouter.use(requestAttestation);
 userRouter.use(pay);
+userRouter.use(metamap);
 
 userRouter.use(
   // eslint-disable-next-line import/no-named-as-default-member
