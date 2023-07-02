3da2450012ebcdf0dedb46c7eda3379cc1bcb8dd
metabaronespinosa <axnotliztac@gmail.com>
2023-07-01 20:06:06 -0600

    test fb 5

--- src/backend/endpoints/user/metamap.ts
+++ src/backend/endpoints/user/metamap.ts
@@ -1,11 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { Request, Response, Router } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from 'firebase/app';
+
+import { getFirestore, doc, setDoc } from 'firebase/firestore';
 
 import { logger } from '../../utilities/logger';
-import { Session, sessionMiddleware } from '../../utilities/sessionStorage';
-import { paths } from '../paths';
-import { addClaim } from '../../utilities/credentialStorage';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAAwR5GvEUi3lLWy9bb1tz65jhvHI3vufc',
@@ -16,12 +16,22 @@ const firebaseConfig = {
   appId: '1:777447831295:web:6a987d7c8b307ecef43eca',
 };
 
-const app = initializeApp(firebaseConfig);
+const apps = getApps();
 
+export const firebase =
+  apps.length > 0 ? apps[0] : initializeApp(firebaseConfig);
+
+export const firestore = getFirestore(firebase);
+
+// WH SECRET oEI07pv3tHbQ1069H61r35C0K
 async function handler(req: Request, res: Response): Promise<void> {
   try {
+    const { flowId } = req.body;
+
+    const docRef = await setDoc(doc(firestore, 'metamap', flowId), req.body);
+
     res.json({
-      true: true,
+      docRef,
     });
   } catch (error) {
     logger.error(error);
