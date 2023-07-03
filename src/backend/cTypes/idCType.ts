c4fdbc527c84035a35aa74ef4dffffb21c1c79c6
metabaronespinosa <axnotliztac@gmail.com>
2023-07-03 07:17:52 -0600

    trigger deploy 2

--- src/backend/cTypes/idCType.ts
+++ src/backend/cTypes/idCType.ts
@@ -10,7 +10,7 @@ export const idCType: ICType = {
       type: 'string',
     },
   },
-  title: 'INE ID',
+  title: 'Authorization',
   type: 'object',
   $id: 'kilt:ctype:0xdf952230c87e7fbdc8503a394540c8e99029180611e7107c4eee33f67f57aa57',
 };
