1d5880ed2438c3472a2ef4ebee6d8c0e2a2516bc
Metabaron <axnotliztac@gmail.com>
2023-08-16 14:30:16 -0700

    fix build

--- src/app/admin/votation/page.tsx
+++ src/app/admin/votation/page.tsx
@@ -3,33 +3,43 @@
 import React from 'react';
 import { useCallback, useEffect, useState } from 'react';
 import { useParams, useRouter } from 'next/navigation';
-import ReactJson from 'react-json-view';
+import _ReactJson from 'react-json-view';
 import { Box, Button } from '@chakra-ui/react';
 import ky from 'ky';
 
+const ReactJson = React.lazy(() => import('react-json-view'));
+
 export default function Votation() {
-  const [votation, setvotation] = useState({});
+  const [votation, setvotation] = useState<any>(null);
 
   useEffect(() => {
     const init = async () => {
-      setvotation(await ky.get(`/api/vote`).json());
+      const r = await ky.get(`/api/vote`).json();
+
+      setTimeout(() => {
+        setvotation(r);
+      }, 1000);
     };
 
-    init();
+    if (
+      typeof window !== 'undefined' &&
+      typeof window?.document !== 'undefined'
+    )
+      init();
   }, []);
 
-  if (!votation)
+  if (votation)
     return (
-      <section className="max-w-full overflow-x-auto p-4 border">
-        <p>...</p>
-      </section>
+      <>
+        <section className="max-w-full overflow-x-auto p-4 border">
+          <ReactJson src={votation} />
+        </section>
+      </>
     );
 
   return (
-    <>
-      <section className="max-w-full overflow-x-auto p-4 border">
-        <ReactJson src={votation} />
-      </section>
-    </>
+    <section className="max-w-full overflow-x-auto p-4 border">
+      <p>...</p>
+    </section>
   );
 }
