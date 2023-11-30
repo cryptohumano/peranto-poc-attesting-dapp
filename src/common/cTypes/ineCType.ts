7e4b6e02b299fcfbc4e58295ee9a6f26a12de20c
metabaronespinosa <axnotliztac@gmail.com>
2023-11-30 15:43:29 -0600

    new ctype

--- src/common/cTypes/ineCType.ts
+++ src/common/cTypes/ineCType.ts
@@ -3,18 +3,82 @@ import type { ICType } from '@kiltprotocol/sdk-js';
 export const ineCType: ICType = {
   $schema: 'ipfs://bafybeiah66wbkhqbqn7idkostj2iqyan2tstc4tpqt65udlhimd7hcxjyq/',
   additionalProperties: false,
-  properties: {
-    dateofbirth: {
-      type: 'string',
+  "properties": {
+    "cic": {
+      "type": "string"
     },
-    fullname: {
-      type: 'string',
+    "citizenId": {
+      "type": "string"
     },
-    ineid: {
-      type: 'string',
+    "country": {
+      "maxLength": 30,
+      "minLength": 1,
+      "type": "string"
+    },
+    "curp": {
+      "type": "string"
+    },
+    "dateOfBirth": {
+      "type": "string"
+    },
+    "documentIssueDate": {
+      "type": "string"
+    },
+    "documentValidity": {
+      "type": "string"
+    },
+    "electorKey": {
+      "type": "string"
+    },
+    "fullName": {
+      "maxLength": 100,
+      "minLength": 1,
+      "type": "string"
+    },
+    "gender": {
+      "type": "string"
+    },
+    "governmentDbValidationIne": {
+      "type": "string"
+    },
+    "governmentDbValidationRenapo": {
+      "type": "string"
+    },
+    "idType": {
+      "type": "string"
+    },
+    "locality": {
+      "type": "string"
+    },
+    "mrz": {
+      "type": "string"
+    },
+    "municipality": {
+      "type": "string"
+    },
+    "municipalityName": {
+      "type": "string"
+    },
+    "ocr": {
+      "type": "string"
+    },
+    "registrationDate": {
+      "type": "string"
+    },
+    "residenceAddress": {
+      "type": "string"
+    },
+    "section": {
+      "type": "string"
+    },
+    "state": {
+      "type": "string"
+    },
+    "stateName": {
+      "type": "string"
     },
   },
-  title: 'Perantotestine',
+  title: 'Peranto_ID_MX',
   type: 'object',
-  $id: 'kilt:ctype:0xa308e706b9326580bc4e90a48933b382466344ddd18059f5b09ab3607cfd92a8',
+  $id: 'kilt:ctype:0x651f425ded898bd7a9f3e6bd28966ac53f9a871de7b3a9918720cfb4a2996cde',
 };
