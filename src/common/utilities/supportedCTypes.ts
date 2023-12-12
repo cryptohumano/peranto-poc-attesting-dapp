a86fa9d6755c0efc12c1f2fffdfd2da4cf2a9ad1
Metabaron <axnotliztac@gmail.com>
2023-12-12 14:48:32 -0600

    return fee to 2 from 20

--- src/common/utilities/supportedCTypes.ts
+++ src/common/utilities/supportedCTypes.ts
@@ -15,7 +15,7 @@ export const supportedCTypes: Record<SupportedCType, ICType> = {
 };
 
 export const kiltCost: Record<SupportedCType, number> = {
-  ine: 20,
+  ine: 2,
   email: 2,
   twitter: 3,
 };
