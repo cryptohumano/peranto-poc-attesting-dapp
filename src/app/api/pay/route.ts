d6752d04f8676985be6b9220ae8831536a1d1a6d
Metabaron <axnotliztac@gmail.com>
2023-12-13 22:59:08 -0600

    fix build 4

--- src/app/api/pay/route.ts
+++ src/app/api/pay/route.ts
@@ -10,6 +10,7 @@ import { addClaim } from '@/common/utilities/credentialStorage';
 import { StatusCodes } from 'http-status-codes';
 import { sessionMiddleware } from '@/common/utilities/sessionStorage';
 import { encryptMessageBody } from '@/common/utilities/encryptMessage';
+import { ConfigService } from '@kiltprotocol/sdk-js';
 
 export async function POST(request: Request) {
   // implement your payment logic here
@@ -41,6 +42,12 @@ export async function POST(request: Request) {
         html: `<div><h2>New attestation request with id: ${id}</h2><br /><p><a href="https://app.peranto.xyz/admin/attestation/credential/${id}">https://app.peranto.xyz/admin/attestation/credential/${id}</a></p></div>`,
       },
     });
+
+    const api = ConfigService.get('api')
+
+    const txTransfer = api.tx.balances.transfer("4qTRExDVsdgu6UexcZfhb8ZfgjhaotLLe8H12KyE4wKU3URY", 2)
+
+    console.log("AAAAAA", txTransfer)
 
     // return new NextResponse(null, { status: StatusCodes.NO_CONTENT })
     return new NextResponse(JSON.stringify(output))
